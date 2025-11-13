ed0aec7d081e0f2831ed9f46894a1f15b0aaf169
Muzovaka <T.Muzovaka@exeter.ac.uk>
2025-11-13 13:47:10 +0000

    Another commit

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoProject project.
+URL configuration for board project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
